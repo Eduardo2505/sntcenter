d88471a33bf5e0e6471040931850daa43ff0db9a
Eduardo2505 <eduardopadillacz@gmail.com>
2018-01-12 19:07:02 -0600

    base

---
+++ config/cargainicial.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sntcenter_db`.`sucursal` (`nombre`, `direccion`) VALUES ('SNT Center', 'Pedro Moreno #81ª');
+INSERT INTO `sntcenter_db`.`nutriologo` (`nombre`, `apellidos`, `cedula`, `email`, `password`, `idsucursal`) VALUES ('Luis Alberto', 'Gómez Martin', 'AFFEJ/NABBA, CICD, WFF e INAD', 'gomezluisnutricion@hotmail.com', '12345', '1');
+
